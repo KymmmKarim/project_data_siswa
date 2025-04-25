7a199cca1e2d0b7bce805deae17a1db0cba6d3c0
fadhlan <ffadhlilah881@gmail.com>
2025-04-25 14:58:38 +0700

    feat(APS-1):create beranda

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/beranda', function () {
+    return view('beranda');
+});
+Route::get('/kontak', function () {
+    return view('kontak');
+});
+Route::get('/tentang', function () {
+    return view('tentang');
+});
